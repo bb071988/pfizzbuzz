384c0395b5228b7dd6ea0217ab781a1a32668ba6
Bob Braico <bb071988@gmail.com>
2015-02-15 04:22:46 +0000

    initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,57 @@
+## fizz buzz
+
+
+# -*- coding: cp1252 -*-
+
+import sys
+
+upperLimit = None
+
+x=1
+
+inputList =[1,2,3,4,5,6,7,8,9,10]
+outputList =['1','2','fizz','4','buzz','fizz','7','8','fizz',
+             'buzz','11','fizz','13','14','fizzbuzz']
+
+
+
+def fizzbuzz(x):
+
+    if(x % 3==0 and x % 5 ==0):
+        return 'fizz buzz'
+    elif(x % 3==0):
+        return 'fizz'
+    elif(x % 5 ==0):
+        return 'buzz'
+    else:
+        return str(x)
+
+def test():
+
+    for i, item in enumerate(inputList):
+        print('Asserting that {} = {}'.format(fizzbuzz(item),outputList[i]))
+        assert(fizzbuzz(item) == outputList[i])
+
+
+while(upperLimit == None):
+    if len(sys.argv) >= 2:
+        tempLimit = sys.argv[1]
+        upperLimit = int(tempLimit)
+
+    else:
+        tempLimit = raw_input("Please enter an integer to fizzbuzz  ")
+        try:
+            upperLimit = int(tempLimit)
+        except:
+            print("invalid input ")
+            
+        
+test()
+    
+while(x < upperLimit + 1):
+
+    print(fizzbuzz(x))
+
+    x+=1
+    
+
